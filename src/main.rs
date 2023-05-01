52a45653c0b529e7e62366e1ce9f0fdfe1740652
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2023-05-01 12:30:09 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::fmt::{Display, Formatter};
+use crate::pixel::Pixel;
+use crate::qoi_header::QoiHeader;
+
+mod qoi_header;
+mod pixel;
+mod qoi_op_util;
+
 fn main() {
     println!("Hello, world!");
 }
